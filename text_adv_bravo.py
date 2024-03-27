047e1687cfc0169ddf3cafc8b5139148398246e4
BenjaminHauf <benx@ben-hauf.de>
2024-03-27 13:38:47 +0100

    add decision template

---
+++ text_adv_bravo.py
@@ -0,0 +1,12 @@
+intro_decision = ""
+
+print(intro_decision, "green")
+decision = input("Choose: ")
+if decision.lower == "1":
+    print("1")
+elseif decision.lower == "2"
+    print("2")
+elseif decision.lower == "3"
+    print("3")
+
+    
