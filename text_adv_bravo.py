1ef7a9a911c265632926f0fef90b1351060da355
BenjaminHauf <benx@ben-hauf.de>
2024-03-27 14:57:43 +0100

    starting

--- text_adv_bravo.py
+++ text_adv_bravo.py
@@ -1,12 +1,27 @@
-intro_decision = ""
+from stringcolor import cs
+import time
 
-print(intro_decision, "green")
+def printc(string, color="white"):
+    # string first, then color optional - default white
+    print(cs(string,color))
+
+
+intro="Introduction:
+printc(intro, green)
+printc("You find yourself washed ashore on a mysterious island after a violent storm wreaks havoc on your ship. As you regain consciousness, you realize you're not alone. Pirate remnants litter the beach, and the distant sounds of waves crashing against the rocky cliffs echo ominously. Determined to survive, you venture deeper into the island's uncharted territory, unaware of the dangers lurking within.")
+time.sleep(5)
+printc("Act 1: The Encounter", green)
+printc("As you explore the island, you stumble upon a ragtag group of pirates who have made this island their hideout. They greet you with suspicion, questioning your motives. With no means of escape, you strike a deal with the pirates: help them locate a legendary treasure rumored to be hidden somewhere on the island, and they'll assist you in returning home.")
+printc("fight | help | wait like a good german citizen")
 decision = input("Choose: ")
-if decision.lower == "1":
-    print("1")
-elseif decision.lower == "2"
-    print("2")
-elseif decision.lower == "3"
-    print("3")
+if decision.lower == "fight":
+    printc("YOU ARE DEAD")
+    #restart?
+elif decision.lower == "help":
+    printc("You are helping the pirates and agree to their conditions")
+    #next print + new input + new if elif
+elif decision.lower == "negotiate":
+    printc("You tell the pirates that you will help them if they give you a part of the tresure")
+    #next print + new input + new if elif
 
     
